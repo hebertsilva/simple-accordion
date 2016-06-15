2429cf1a70771b4764807326abc37959c1fc38f2
Hebert Silva <hebertpereirasilva@gmail.com>
2016-06-15 12:15:47 -0300

    close all accordion and display to current

--- README.md
+++ README.md
@@ -15,10 +15,21 @@ very simple to use
     <div data-simple-accordion-content>*content</div>
 </div>
 ```
+or
+
+```html
+<div data-simple-accordion>
+    <span data-simple-accordion-toggle>button</span>
+    
+	<div data-simple-accordion-content="close">*content</div>    
+</div>
+```
 
 # additional
 
 `data-simple-accordion-toggle` when active add class `active` for custom style
+
+`data-simple-accordion-content="close"` hides all and displays the current
 
 
 Feel free to modify
--- simple.accordion.js
+++ simple.accordion.js
@@ -1,19 +1,45 @@
 (function($) {
     var SimpleAccordion = function( el ) {
-        this.el = el;
-        this.accordion = $('[data-simple-accordion]', this.el)
-        this.accordionToggle = $('[data-simple-accordion-toggle]', this.el);
-        this.accordionContent = $('[data-simple-accordion-content]', this.el);
+        this.accordion = el;
+
+        // all accordion
+        this.accordionContentAll = $('[data-simple-accordion-content]');
+        this.accordionToggleAll = $('[data-simple-accordion-toggle]');
+
+        // single accordion current
+        this.accordionToggle = $('[data-simple-accordion-toggle]', this.accordion);
+        this.accordionContent = $('[data-simple-accordion-content]', this.accordion);
         
+        // event
         this.accordionToggle.on('click', $.proxy(this, 'showAccordionHandler'));
     }
 
     SimpleAccordion.prototype = {
         showAccordionHandler: function( event ) {
-           event.preventDefault();
-           var target = $(event.currentTarget);
-           target.toggleClass('active');
-           target.parents(this.accordion).find(this.accordionContent).stop().slideToggle('fast');
+            event.preventDefault();
+            var that = this,
+                target = $(event.currentTarget);
+
+            if ( this.accordion.data('simple-accordion') === 'close' ) {
+                if ( target.parents(this.accordion).find(this.accordionContent).is(':visible') ) {
+                    this.showDefaultAccordion( target );
+                } else {
+                    this.hideAllAccordion( target );
+                }
+            } else {
+                this.showDefaultAccordion( target );
+            }
+        },
+
+        showDefaultAccordion: function( target ) {
+            target.toggleClass('active');
+            target.parents(this.accordion).find(this.accordionContent).stop().slideToggle('fast');
+        },
+
+        hideAllAccordion: function( target ) {
+            this.accordionToggleAll.removeClass('active');
+            this.accordionContentAll.stop().slideUp('fast');
+            this.showDefaultAccordion( target );
         }
     }
 
